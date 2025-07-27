ff5c910337ef9eb7fa0480b07f17fb5c26fafa75
Noel De Martin <noel@noeldemartin.com>
2025-07-27 11:22:09 +0200

    Fix CI

--- src/controls.test.ts
+++ src/controls.test.ts
@@ -1,19 +1,28 @@
-import { describe, expectTypeOf, it } from 'vitest';
-import { showModal } from '@noeldemartin/vue-modals/controls';
-import NoResponse from '@noeldemartin/vue-modals/testing/fixtures/NoResponse.vue';
-import LiteralResponse from '@noeldemartin/vue-modals/testing/fixtures/LiteralResponse.vue';
-import ObjectResponse from '@noeldemartin/vue-modals/testing/fixtures/ObjectResponse.vue';
+import { describe, it } from 'vitest';
+import { type GetModalResponse } from '@noeldemartin/vue-modals/controls';
+import { type Expect, tt } from '@noeldemartin/testing';
+import type { Constructor, Equals } from '@noeldemartin/utils';
+import type { Component } from 'vue';
 
 describe('controls', () => {
 
-    it('infers response types', () => {
-        expectTypeOf(showModal(NoResponse)).toEqualTypeOf<Promise<{ dismissed: boolean }>>();
-        expectTypeOf(showModal(LiteralResponse)).toEqualTypeOf<
-            Promise<{ dismissed: false; response: string } | { dismissed: true; response?: undefined }>
-        >();
-        expectTypeOf(showModal(ObjectResponse)).toEqualTypeOf<
-            Promise<{ dismissed: false; answer: string } | { dismissed: true; answer?: undefined }>
-        >();
-    });
+    it(
+        'infers response types',
+        tt<
+            | Expect<Equals<GetModalResponse<Constructor<Component>>, { dismissed: boolean }>>
+            | Expect<
+                  Equals<
+                      GetModalResponse<Constructor<{ $emit: (event: 'close', payload: string) => void }>>,
+                      { dismissed: false; response: string } | { dismissed: true; response?: undefined }
+                  >
+              >
+            | Expect<
+                  Equals<
+                      GetModalResponse<Constructor<{ $emit: (event: 'close', payload: { answer: string }) => void }>>,
+                      { dismissed: false; answer: string } | { dismissed: true; answer?: undefined }
+                  >
+              >
+        >(),
+    );
 
 });
--- src/testing/fixtures/LiteralResponse.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <!-- ... -->
-</template>
-
-<script setup lang="ts">
-defineEmits<{ close: [string] }>();
-</script>
